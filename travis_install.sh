266fe6218e11f8303864621d46ab90e2c61dbfd6
Torsten Raudssus <torsten@raudss.us>
2013-09-04 22:57:45 +0200

    test

--- travis_install.sh
+++ travis_install.sh
@@ -15,4 +15,6 @@ tar xvzf gdal-$GDAL_VERSION.travis.$LSB_RELEASE.tar.gz -C /
 cd $REPO_DIR
 
 ./autogen.sh
-./configure --with-geosconfig=/home/travis/geos-$GEOS_VERSION/bin/geos-config --with-gdalconfig=/home/travis/gdal-$GDAL_VERSION/bin/gdal-config
+./configure \
+  --with-geosconfig=/home/travis/geos-$GEOS_VERSION/bin/geos-config \
+  --with-gdalconfig=/home/travis/gdal-$GDAL_VERSION/bin/gdal-config
