d3d139b2dc3891c8a5effb0dc3ae34190c2519a6
Torsten Raudssus <torsten@raudss.us>
2013-09-05 00:12:51 +0200

    test

--- travis_install.sh
+++ travis_install.sh
@@ -4,13 +4,32 @@ REPO_DIR=$( pwd )
 
 set -x
 
+sudo apt-get update
+sudo apt-get remove postgresql-9.1
+sudo apt-get install -qq build-essential flex autoconf libtool gfortran postgresql-server-dev-all xsltproc libjson0-dev libproj-dev dblatex xsltproc libcunit1-dev libcunit1 docbook-xsl docbook-mathml
+
 export LSB_RELEASE=$( lsb_release -rs )
-export LD_LIBRARY_PATH="/home/travis/geos-$GEOS_VERSION/lib:/home/travis/gdal-$GDAL_VERSION/lib"
+
+export GEOS_PATH="/home/travis/geos-$GEOS_VERSION"
+export PATH="$GEOS_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$GEOS_PATH/lib"
+
+export GDAL_PATH="/home/travis/gdal-$GDAL_VERSION"
+export PATH="$GDAL_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$GDAL_PATH/lib:$LD_LIBRARY_PATH"
+
+export POSTGRES_PATH="/home/travis/postgresql-$POSTGRES_VERSION"
+export PATH="$POSTGRES_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$POSTGRES_PATH/lib:$LD_LIBRARY_PATH"
 
 wget -q http://stardestroyer.de/travis/geos-$GEOS_VERSION.travis.$LSB_RELEASE.tar.gz
 tar xvzf geos-$GEOS_VERSION.travis.$LSB_RELEASE.tar.gz -C /
+
 wget -q http://stardestroyer.de/travis/gdal-$GDAL_VERSION.travis.$LSB_RELEASE.tar.gz
 tar xvzf gdal-$GDAL_VERSION.travis.$LSB_RELEASE.tar.gz -C / 
+
+wget -q http://stardestroyer.de/travis/postgresql-$POSTGRES_VERSION.travis.$LSB_RELEASE.tar.gz
+tar xvzf postgresql-$POSTGRES_VERSION.travis.$LSB_RELEASE.tar.gz -C / 
 
 cd $REPO_DIR
 
@@ -18,3 +37,4 @@ cd $REPO_DIR
 ./configure \
   --with-geosconfig=/home/travis/geos-$GEOS_VERSION/bin/geos-config \
   --with-gdalconfig=/home/travis/gdal-$GDAL_VERSION/bin/gdal-config
+  --with-pgconfig=/home/travis/postgresql-$POSTGRES_VERSION/bin/pg_config
