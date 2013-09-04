a3b5aadb9b2f766e18913ca64dcf56e1fe218658
Torsten Raudssus <torsten@raudss.us>
2013-09-04 22:14:43 +0200

    test

--- travis_install.sh
+++ travis_install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-set +x
+REPO_DIR=$( pwd )
+
+set -x
 
 export LSB_RELEASE=$( lsb_release -rs )
 
 wget -q http://stardestroyer.de/travis/geos-$GEOS_VERSION.travis.$LSB_RELEASE.tar.gz
-tar xvzf geos-$GEOS_VERSION.travis.$LSB_RELEASE.tar.gz -C / 
-sudo ln -s /home/travis/geos-$GEOS_VERSION/lib/libgeos_c.so.1.8.0 /usr/lib/libgeos_c.so.1
+tar xvzf geos-$GEOS_VERSION.travis.$LSB_RELEASE.tar.gz -C /
 wget -q http://stardestroyer.de/travis/gdal-$GDAL_VERSION.travis.$LSB_RELEASE.tar.gz
 tar xvzf gdal-$GDAL_VERSION.travis.$LSB_RELEASE.tar.gz -C / 
+
+cd $REPO_DIR
+
 LD_LIBRARY_PATH="/home/travis/geos-$GEOS_VERSION/lib:/home/travis/gdal-$GDAL_VERSION/lib"
 ./configure --with-geosconfig=/home/travis/geos-$GEOS_VERSION/bin/geos-config --with-gdalconfig=/home/travis/gdal-$GDAL_VERSION/bin/gdal-config
